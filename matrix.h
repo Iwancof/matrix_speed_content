947104c7dcbbda852ce86f94589eccdd6f68b4c2
Iwancof <rock.iwanof@gmail.com>
2022-11-21 18:58:51 +0900

    faster!

--- matrix.h
+++ matrix.h
@@ -153,13 +153,13 @@ static inline void matrix_mult_per_block(
 
 #ifdef RIGHT_TRANSPOSE
 
-#pragma omp parallel for private(cache_block_x, move) num_threads(PARALLEL)    \
+#pragma omp parallel for private(cache_block_y, move) num_threads(PARALLEL)    \
     schedule(static)
-  for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
-       cache_block_y++) { // for write cache
+  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
     const int thread_index = omp_get_thread_num();
     matrix *const thread_dest = thread_memo[thread_index];
-    for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
+    for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
+         cache_block_y++) { // for write cache
       for (move = 0; move < SUPER_SIZE; move++) {
         BLOCK_MULT(&left->blocks[cache_block_y][move],
                    &right->blocks[cache_block_x][move],
