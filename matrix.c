fecfcffe063d68151906039287fb97480a1a90e2
Iwancof <rock.iwanof@gmail.com>
2022-11-21 19:39:54 +0900

    move mult from matrix.h to matrix.c

--- matrix.c
+++ matrix.c
@@ -127,8 +127,8 @@ void block_add(const block *const restrict left,
 #ifdef RIGHT_TRANSPOSE
 #ifdef USE_SIMD
   // right and dest is transposed.
-  for(INDEX_TYPE x = 0; x < BLOCK_SIZE; x+=4) {
-    for(INDEX_TYPE y = 0; y < BLOCK_SIZE; y++) {
+  for (INDEX_TYPE x = 0; x < BLOCK_SIZE; x += 4) {
+    for (INDEX_TYPE y = 0; y < BLOCK_SIZE; y++) {
       SIMD_TYPE left_fragment, right_fragment;
       left_fragment[0] = left->element[x + 0][y];
       left_fragment[1] = left->element[x + 1][y];
@@ -150,7 +150,6 @@ void block_add(const block *const restrict left,
 #else // RIGHT_TRANSPOSE
 #error if you want it, write it yourself.
 #endif // RIGHT_TRANSPOSE
-       
 }
 
 #ifdef BLOCK_MULT_FUNC
@@ -439,3 +438,94 @@ inline void BLOCK_MULT(const block *const restrict left,
 #endif // RIGHT_TRANSPOSE
 }
 #endif // BLOCK_MULT_FUNC
+
+void matrix_mult_per_block(const matrix *const restrict left,
+                           const matrix *const restrict right,
+                           matrix *const restrict dest,
+                           matrix *const restrict thread_memo[PARALLEL]) {
+  INDEX_TYPE cache_block_x, cache_block_y, move, block_x, block_y;
+
+#ifdef RIGHT_TRANSPOSE
+
+#pragma omp parallel for private(cache_block_y, move) num_threads(PARALLEL)    \
+    schedule(static)
+  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
+    const int thread_index = omp_get_thread_num();
+    matrix *const thread_dest = thread_memo[thread_index];
+    for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
+         cache_block_y++) { // for write cache
+      for (move = 0; move < SUPER_SIZE; move++) {
+        BLOCK_MULT(&left->blocks[cache_block_y][move],
+                   &right->blocks[cache_block_x][move],
+                   &thread_dest->blocks[cache_block_x][cache_block_y]);
+      }
+    }
+  }
+
+  puts("reducting");
+
+#pragma omp parallel for private(cache_block_x, block_x, block_y)              \
+    num_threads(PARALLEL) schedule(static)
+  for (cache_block_y = 0; cache_block_y < SUPER_SIZE; cache_block_y++) {
+    for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
+      block *const dest_block = &dest->blocks[cache_block_y][cache_block_x];
+      for (int i = 0; i < PARALLEL; i++) {
+        const block *const from =
+            &thread_memo[i]->blocks[cache_block_y][cache_block_x];
+        for (block_x = 0; block_x < BLOCK_SIZE; block_x++) {
+          for (block_y = 0; block_y < BLOCK_SIZE; block_y++) {
+            dest_block->element[block_y][block_x] +=
+                from->element[block_y][block_x];
+          }
+        }
+      }
+    }
+  }
+
+#else // RIGHT_TRANSPOSE
+
+#pragma omp parallel for private(cache_block_y, move) num_threads(PARALLEL)    \
+    schedule(static)
+  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
+    int thread_index = omp_get_thread_num();
+    matrix *thread_dest = thread_memo[thread_index];
+    for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
+         cache_block_y++) { // for write cache
+      for (move = 0; move < SUPER_SIZE; move++) {
+        BLOCK_MULT(&left->blocks[cache_block_y][cache_block_x],
+                   &right->blocks[cache_block_x][move],
+                   &thread_dest->blocks[cache_block_y][move]);
+      }
+    }
+  }
+
+#pragma omp parallel for private(cache_block_x, block_x, block_y)              \
+    num_threads(PARALLEL) schedule(static)
+  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
+    for (cache_block_y = 0; cache_block_y < SUPER_SIZE; cache_block_y++) {
+      block *dest_block = &dest->blocks[cache_block_y][cache_block_x];
+      for (int i = 0; i < PARALLEL; i++) {
+        block *from = &thread_memo[i]->blocks[cache_block_y][cache_block_x];
+        for (block_x = 0; block_x < BLOCK_SIZE; block_x++) {
+          for (block_y = 0; block_y < BLOCK_SIZE; block_y++) {
+            dest_block->element[block_y][block_x] +=
+                from->element[block_y][block_x];
+          }
+        }
+      }
+    }
+  }
+#endif
+}
+
+void matrix_mult_vanilla(matrix *left, matrix *right, matrix *dest) {
+  // TODO: RIGHT_TRANSPOSE
+  for (INDEX_TYPE x = 0; x < SUPER_SIZE; x++) {
+    for (INDEX_TYPE y = 0; y < SUPER_SIZE; y++) {
+      for (INDEX_TYPE i = 0; i < SUPER_SIZE; i++) {
+        BLOCK_MULT(&left->blocks[y][i], &right->blocks[i][x],
+                   &dest->blocks[y][x]);
+      }
+    }
+  }
+}
--- matrix.h
+++ matrix.h
@@ -151,93 +151,9 @@ void BLOCK_MULT(const block *const restrict left,
 /// ASSUME: left, right, dest has been allocated.
 /// ASSUME: dest has been initialized.
 
-static inline void matrix_mult_per_block(
-    const matrix *const restrict left, const matrix *const restrict right,
-    matrix *const restrict dest, matrix *const restrict thread_memo[PARALLEL]) {
-  INDEX_TYPE cache_block_x, cache_block_y, move, block_x, block_y;
+void matrix_mult_per_block(const matrix *const restrict left,
+                           const matrix *const restrict right,
+                           matrix *const restrict dest,
+                           matrix *const restrict thread_memo[PARALLEL]);
 
-#ifdef RIGHT_TRANSPOSE
-
-#pragma omp parallel for private(cache_block_y, move) num_threads(PARALLEL)    \
-    schedule(static)
-  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
-    const int thread_index = omp_get_thread_num();
-    matrix *const thread_dest = thread_memo[thread_index];
-    for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
-         cache_block_y++) { // for write cache
-      for (move = 0; move < SUPER_SIZE; move++) {
-        BLOCK_MULT(&left->blocks[cache_block_y][move],
-                   &right->blocks[cache_block_x][move],
-                   &thread_dest->blocks[cache_block_x][cache_block_y]);
-      }
-    }
-  }
-
-  puts("reducting");
-
-#pragma omp parallel for private(cache_block_x, block_x, block_y)              \
-    num_threads(PARALLEL) schedule(static)
-  for (cache_block_y = 0; cache_block_y < SUPER_SIZE; cache_block_y++) {
-    for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
-      block *const dest_block = &dest->blocks[cache_block_y][cache_block_x];
-      for (int i = 0; i < PARALLEL; i++) {
-        const block *const from =
-            &thread_memo[i]->blocks[cache_block_y][cache_block_x];
-        for (block_x = 0; block_x < BLOCK_SIZE; block_x++) {
-          for (block_y = 0; block_y < BLOCK_SIZE; block_y++) {
-            dest_block->element[block_y][block_x] +=
-                from->element[block_y][block_x];
-          }
-        }
-      }
-    }
-  }
-
-#else // RIGHT_TRANSPOSE
-
-#pragma omp parallel for private(cache_block_y, move) num_threads(PARALLEL)    \
-    schedule(static)
-  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
-    int thread_index = omp_get_thread_num();
-    matrix *thread_dest = thread_memo[thread_index];
-    for (cache_block_y = 0; cache_block_y < SUPER_SIZE;
-         cache_block_y++) { // for write cache
-      for (move = 0; move < SUPER_SIZE; move++) {
-        BLOCK_MULT(&left->blocks[cache_block_y][cache_block_x],
-                   &right->blocks[cache_block_x][move],
-                   &thread_dest->blocks[cache_block_y][move]);
-      }
-    }
-  }
-
-#pragma omp parallel for private(cache_block_x, block_x, block_y)              \
-    num_threads(PARALLEL) schedule(static)
-  for (cache_block_x = 0; cache_block_x < SUPER_SIZE; cache_block_x++) {
-    for (cache_block_y = 0; cache_block_y < SUPER_SIZE; cache_block_y++) {
-      block *dest_block = &dest->blocks[cache_block_y][cache_block_x];
-      for (int i = 0; i < PARALLEL; i++) {
-        block *from = &thread_memo[i]->blocks[cache_block_y][cache_block_x];
-        for (block_x = 0; block_x < BLOCK_SIZE; block_x++) {
-          for (block_y = 0; block_y < BLOCK_SIZE; block_y++) {
-            dest_block->element[block_y][block_x] +=
-                from->element[block_y][block_x];
-          }
-        }
-      }
-    }
-  }
-#endif
-}
-
-static inline void matrix_mult_vanilla(matrix *left, matrix *right,
-                                       matrix *dest) {
-  // TODO: RIGHT_TRANSPOSE
-  for (INDEX_TYPE x = 0; x < SUPER_SIZE; x++) {
-    for (INDEX_TYPE y = 0; y < SUPER_SIZE; y++) {
-      for (INDEX_TYPE i = 0; i < SUPER_SIZE; i++) {
-        BLOCK_MULT(&left->blocks[y][i], &right->blocks[i][x],
-                   &dest->blocks[y][x]);
-      }
-    }
-  }
-}
+void matrix_mult_vanilla(matrix *left, matrix *right, matrix *dest);
